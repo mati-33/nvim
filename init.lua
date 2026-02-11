d9b79869a9b111404bb3ebf9d280389baeaf9757
mati <mateusz.meksula@gmail.com>
2026-02-11 22:30:57 +0100

    conform and stylua formatting

--- init.lua
+++ init.lua
@@ -8,15 +8,14 @@ require("keymaps")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-    change_detection = {notify = false},
+    change_detection = { notify = false },
 })
-
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,15 @@
 return {
     "neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup()
         require("mason-tool-installer").setup({
-            ensure_installed = {"lua_ls"}
+            ensure_installed = { "lua_ls", "basedpyright", "gopls" },
         })
-    end
+    end,
 }
