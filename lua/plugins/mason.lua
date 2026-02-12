4aaff12f27464d6d59b52cf7064123afc190510b
mati <mateusz.meksula@gmail.com>
2026-02-12 22:16:58 +0100

    mason-tool-installer opts instead of config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,19 +5,16 @@ return {
     },
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
-        config = function()
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                    --lsp
-                    "lua-language-server",
-                    "ty",
-                    "gopls",
-                    --formatters
-                    "ruff",
-                    "stylua",
-                    --linters
-                },
-            })
-        end,
+        opts = {
+            ensure_installed = {
+                --lsp
+                "lua-language-server",
+                "ty",
+                "gopls",
+                --formatters
+                "ruff",
+                "stylua",
+            },
+        },
     },
 }
