684bbc7deb947f87ef320ac88f412cb2a2c49d21
mati <mateusz.meksula@gmail.com>
2026-02-12 21:50:04 +0100

    conform more formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,8 @@ return {
     opts = {
         formatters_by_ft = {
             lua = { "stylua" },
+            go = { "gofmt" },
+            python = { "ruff_format", "ruff_organize_imports" },
         },
         format_on_save = {
             -- These options will be passed to conform.format()
