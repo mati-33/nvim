2c924bcbc8f3df6d1f9f8d8b6d8cfdee81b6d65b
mati <mateusz.meksula@gmail.com>
2026-02-13 22:36:06 +0100

    colorscheme color tweaks

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -5,8 +5,21 @@ return {
     priority = 9001,
     config = function()
         require("rose-pine").setup({
-            styles = {
-                transparency = true,
+            variant = "dawn",
+            palette = {
+                dawn = {
+                    _nc = "#16141f",
+                    base = "#141618",
+                    surface = "#1c1c1c",
+                    overlay = "#182022",
+                    text = "#ffffff",
+                    gold = "#f6c177",
+                    muted = "#6e6a86",
+                    subtle = "#908caa",
+                    highlight_low = "#21202e",
+                    highlight_med = "#403d52",
+                    highlight_high = "#524f67",
+                },
             },
         })
         vim.cmd("colorscheme rose-pine")
